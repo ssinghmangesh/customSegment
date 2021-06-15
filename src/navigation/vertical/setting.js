fcd79b438795d4ba1af93da4da4da6c31cbc9ba2
ssinghmangesh <ssingh.mangesh@gmail.com>
2021-06-15 15:35:07 +0530

    changes nav

---
+++ src/navigation/vertical/setting.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    header: 'Settings',
+  },
+  {
+    title: 'User',
+    icon: 'UserIcon',
+    children: [
+      {
+        title: 'List',
+        route: 'apps-users-list',
+      },
+      {
+        title: 'View',
+        route: { name: 'apps-users-view', params: { id: localStorage.getItem('userId') || 21 } },
+      },
+      {
+        title: 'Edit',
+        route: { name: 'apps-users-edit', params: { id: localStorage.getItem('userId') || 21 } },
+      },
+    ],
+  },
+]
