19b61734a5816f1a06e01bf426b13d86a187f02b
ssinghmangesh <ssingh.mangesh@gmail.com>
2021-06-22 10:59:20 +0530

    changes ui of notification page

--- src/views/notifications/index.vue
+++ src/views/notifications/index.vue
@@ -8,21 +8,36 @@
     >
       Save
     </b-button>
-    <b-form-checkbox
-      v-for="(option, index) in options"
-      :key="index"
-      v-model="selected"
-      class="mb-1"
-      :value="option"
-      switch
+
+    <app-collapse
+      accordion
+      type="border"
     >
-      <span class="h5 ml-1">{{ option }}</span>
-    </b-form-checkbox>
+
+      <app-collapse-item
+        :title="optionsdetails[0]"
+        :key="index"
+        v-for="(optionsdetails, index) in optionsGroup"
+      >
+        <b-form-checkbox
+          v-for="(option, index) in optionsdetails[1]"
+          :key="index"
+          v-model="selected"
+          class="mb-1"
+          :value="option.value"
+          switch
+        >
+          <span class="h5 ml-1">{{ option.label }}</span>
+        </b-form-checkbox>
+      </app-collapse-item>
+    </app-collapse>
   </b-card>
 </template>
 
 <script>
 import { BCard, BFormCheckbox, BButton } from 'bootstrap-vue'
+import AppCollapse from '@core/components/app-collapse/AppCollapse.vue'
+import AppCollapseItem from '@core/components/app-collapse/AppCollapseItem.vue'
 import notifications from './notifications.json'
 
 export default {
@@ -30,6 +45,8 @@ export default {
     BCard,
     BFormCheckbox,
     BButton,
+    AppCollapse,
+    AppCollapseItem,
   },
   data() {
     return {
@@ -45,6 +62,10 @@ export default {
     options() {
       return this.notifications[this.type].options
     },
+    optionsGroup() {
+      const grouped = this.groupBy(this.options, o => o.type)
+      return grouped
+    },
     visible() {
       const flag = this.selected.length === this.prev.length && this.selected.every(el => this.prev.includes(el))
       return flag
@@ -59,10 +80,23 @@ export default {
     await this.update()
   },
   methods: {
+    groupBy(list, keyGetter) {
+      const map = new Map()
+      list.forEach(item => {
+        const key = keyGetter(item)
+        const collection = map.get(key)
+        if (!collection) {
+          map.set(key, [item])
+        } else {
+          collection.push(item)
+        }
+      })
+      return map
+    },
     async update() {
-      const res = await this.$http.post(`/notifications/${this.type}/fetch`)
-      this.selected = [...res.data.data]
-      this.prev = [...res.data.data]
+      // const res = await this.$http.post(`/notifications/${this.type}/fetch`)
+      this.selected = this.options.map(v => v.value)
+      this.prev = this.options.map(v => v.value)
     },
     async send() {
       await this.$http.post(`/notifications/${this.type}/insert`, { selected: this.selected })
