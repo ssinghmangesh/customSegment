c4e6c22df1946353440285d6cffd770472f5c8c8
shimin8 <nimish007gupta@gmail.com>
2021-07-07 14:57:43 +0530

    notifications tab

--- src/views/apps/customers/sidebar.vue
+++ src/views/apps/customers/sidebar.vue
@@ -51,8 +51,11 @@
       </b-tab>
       <b-tab
         v-if="type === 'customers'"
-        title="Notification"
+        title="Notifications"
       >
+        <Notifications
+          :customer="row"
+        />
       </b-tab>
       <b-tab
         v-if="type === 'orders'"
@@ -111,6 +114,7 @@ import OrderProduct from './Sidebar/OrderProduct.vue'
 import DraftOrderProduct from './Sidebar/DraftOrderProduct.vue'
 import CartProduct from './Sidebar/CartProduct.vue'
 import ProductRecommendations from './Sidebar/ProductRecommendations.vue'
+import Notifications from './Sidebar/Notifications.vue'
 
 export default {
   components: {
@@ -128,6 +132,7 @@ export default {
     DraftOrderProduct,
     CartProduct,
     ProductRecommendations,
+    Notifications,
   },
   props: {
     row: {
