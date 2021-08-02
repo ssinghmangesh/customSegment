4b4ae7fa63a7a61aac567cca3df9aae798018c19
ssinghmangesh <ssingh.mangesh@gmail.com>
2021-08-03 02:26:07 +0530

    change in folder name

--- src/router/routes/tagmanager.js
+++ src/router/routes/tagmanager.js
@@ -3,7 +3,7 @@ export default [
     {
       path: '/tags/:type',
       name: 'tags',
-      component: () => import('@/views/apps/tagmanager/index.vue'),
+      component: () => import('@/views/apps/tags/index.vue'),
     },
   ]
   
